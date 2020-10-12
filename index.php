f4f141fee0550f1599e9dbf7bb9b80b115072720
BarbaraGonthier <69505965+BarbaraGonthier@users.noreply.github.com>
2020-10-12 18:02:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ $bike = new Bicycle('blue');
 var_dump($bike);
 
 
-$bike->setCurrentSpeed = 0;
+$bike->setCurrentSpeed(0);
 var_dump($bike);
 
 
 echo $bike->forward();
-echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed . ' km/h' . '<br>';
+echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
 echo $bike->brake();
-echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed . ' km/h' . '<br>';
+echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
 echo $bike->brake();
 
 $rockrider = new Bicycle('yellow');
